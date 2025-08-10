7fd8cbe64f168368e0aef77eb8874e4042abc8b7
LAPTOP-DIU9GIOD\paulo <paullorics@gmail.com>
2025-08-10 18:44:09 -0300

    Comentários

--- Modelo/Livro.py
+++ Modelo/Livro.py
@@ -1,11 +1,12 @@
 from pydantic import BaseModel
 
+# Criação de classe para validação do tipo de dados
 class Book(BaseModel):
-    Id: int
-    title: str
-    price: str
-    availability: str
-    rating: str
-    url: str
-    image_url: str
-    category: str
+    Id: int             # ID do livro
+    title: str          # Título do livro
+    price: str          # Preço
+    availability: str   # Disponibilidade
+    rating: str         # Avaliação
+    url: str            # URL para detalhes do livro
+    image_url: str      # URL da imagem da capa
+    category: str       # Categoria do livro
